467ac159112fa6b3ae9cc287a21a52e4bcc88234
Eduard Balakh <eduardbalakh@gmail.com>
2021-04-01 17:32:20 +0500

    Initial commit

---
+++ src/test/java/com/writemeabook/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writemeabook.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
